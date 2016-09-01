af0f7aa9a3fbad37489799db10297fe02e71dae3
EnesIdrisoglu <idrisoglu_enes@hotmail.com>
2016-09-01 18:26:16 +0300

    Search button

--- Scripts/mobile.js
+++ Scripts/mobile.js
@@ -1,1 +1,13 @@
 /// <reference path="../typings/jquery/jquery.d.ts" />
+
+ $('.control').click( function(){
+     $(".container").css("position","inherit");
+  $('body').addClass('searchMode');
+  $('.input-search').focus();
+});
+
+$('.icon-close').click( function(){
+  $('body').removeClass('searchMode');
+});
+
+  //$(".container").css("position","relative");
