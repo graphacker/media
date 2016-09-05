f5dec42c94fafdc24ac02872a7610ed9ab62e145
EnesIdrisoglu <idrisoglu_enes@hotmail.com>
2016-09-05 18:55:12 +0300

    slider 5 li haber

--- Scripts/mobile.js
+++ Scripts/mobile.js
@@ -24,3 +24,28 @@ $('.icon-close').click( function(){
         sliderWrapper.toggleClass('navSlideWrapperTransform');
     })
 
+
+index=0;	
+var width = $(".slideWrap ul li")[0].getBoundingClientRect().width;
+	$("#sag").click(function(){
+		if(index<4)index++;
+		
+		
+		$(".slideWrap ul").stop().animate({marginLeft:'-' + (index*width)},300);
+		
+		return false;
+	});
+	$("#sol").click(function(){
+		if(index>0)index--;
+		
+		
+		$(".slideWrap ul").stop().animate({marginLeft:'-' + (index*width)},300);
+		
+		return false;
+	});
+    $(".SearchButton").click(function(){
+        $(this).css("transform","scale(80)");
+    });
+    $(".icon-close").click(function(){
+        $(".SearchButton").css("transform","scale(1)");
+    });
